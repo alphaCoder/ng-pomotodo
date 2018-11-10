772dea07a32bee00a37edb80d35754b77e441114
alphaCoder <alphaCoder@users.noreply.github.com>
2018-11-09 23:24:22 -0500

    adding categories

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -8,27 +8,60 @@ import { Todo } from '../models/todo';
 })
 export class TodoListComponent implements OnInit {
 
-  todos: Todo[] = [];
+  todosByCategory: {[key:string]: Todo[]} = {};
+  selectedCategory: string = 'Today';
+  categoryName = '';
   selectedTodo: Todo = null;
+  visible = false;
   constructor() { }
 
   ngOnInit() {
+
+    // todo: add api call
+      this.todosByCategory['Today'] = [];
   }
+
+  addCategory() {
+    const categoryName = this.categoryName.trim();
+    if (categoryName.length > 0) {
+      if (!this.todosByCategory[categoryName]) {
+        this.todosByCategory[categoryName] = [];
+        this.selectedCategory = categoryName; 
+        this.categoryName = '';
+      }
+    }
+  }
+
+  removeCategory(categoryName: string) {
+    delete this.todosByCategory[categoryName];
+    if (this.selectedCategory === categoryName) {
+      this.selectedCategory = null;
+      const keys = Object.keys(this.todosByCategory);
+      if (keys && keys.length > 0) {
+        this.selectedCategory = keys[keys.length-1];
+      }
+    }
+  }
+
+  get categories(): string[] { return Object.keys(this.todosByCategory); }
 
   add() {
     let todo = new Todo();
-    todo.ordinal = this.todos.length;
+    todo.ordinal = this.todosByCategory[this.selectedCategory].length;
     todo.focus = true;
-    this.todos.push(todo);
+    this.todosByCategory[this.selectedCategory].push(todo);
   }
 
   handleEnter(todo: Todo) {
-    if (todo.ordinal === this.todos.length - 1) {
+    if (todo.ordinal === this.todosByCategory[this.selectedCategory].length - 1) {
       this.add();
     }
   }
 
   handleSelectedTask(todo: Todo) {
     this.selectedTodo = todo;
+  }
+  setSelectedCategory(categoryName) {
+    this.selectedCategory = categoryName;
   }
 }
